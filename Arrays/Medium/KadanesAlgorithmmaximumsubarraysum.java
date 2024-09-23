80d102e09cbfc080b8758bf94480ae0bd57c571e
Vishwas9898 <varmavishwas99@gmail.com>
2024-09-23 15:18:00 +0530

    added

---
+++ Arrays/Medium/KadanesAlgorithmmaximumsubarraysum.java
@@ -0,0 +1,30 @@
+package Arrays.Medium;
+
+import java.util.Scanner;
+
+public class KadanesAlgorithmmaximumsubarraysum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+
+        int []arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        int res = maxsum(arr,n);
+
+        System.out.println(res);
+
+    }
+    public static int maxsum(int []arr,int n){
+        int sum =0;
+        int maxsum=0;
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            if(maxsum<sum)maxsum=sum;
+            if(sum<0)sum=0;
+        }
+        return  maxsum;
+    }
+}
